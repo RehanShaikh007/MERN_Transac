95ac9f9490443fd8472d4606c6869f1c507d04e1
RehanShaikh007 <rehanmukhtarshaikh7@gmail.com>
2025-02-07 18:37:21 +0530

    Stats component done

--- frontend/src/components/Stats.jsx
+++ frontend/src/components/Stats.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Stats = () => {
+    const [stats, setStats] = useState({});
+    
+  useEffect(() => {
+    fetchStats();
+  }, [month]);
+
+  const fetchStats = async () => {
+    const response = await axios.get(`http://localhost:4000/api/stats`, {
+      params: { month },
+    });
+    setStats(response.data);
+  };
   return (
-    <div>
-      
+    <div className="mt-4">
+      <h2 className="text-xl font-bold">Statistics</h2>
+      <div className="flex gap-4 mt-2">
+        <div className="p-4 border rounded">
+          <p>Total Sale Amount: {stats.totalSaleAmount}</p>
+        </div>
+        <div className="p-4 border rounded">
+          <p>Total Sold Items: {stats.totalSoldItems}</p>
+        </div>
+        <div className="p-4 border rounded">
+          <p>Total Not Sold Items: {stats.totalNotSoldItems}</p>
+        </div>
+      </div>
     </div>
   )
 }
